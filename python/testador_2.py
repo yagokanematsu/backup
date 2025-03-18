d81a36d057d891c0e7e079f49ff44b13d0472fc8
Yago Kanematsu <yakanematsu@gmail.com>
2025-03-18 01:00:27 -0300

    testador2 atualizado

--- python/testador_2.py
+++ python/testador_2.py
@@ -1,30 +1,11 @@
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def calculator():
-    result = None
-    if request.method == 'POST':
-        try:
-            num1 = float(request.form['num1'])
-            num2 = float(request.form['num2'])
-            operation = request.form['operation']
-            
-            if operation == 'add':
-                result = num1 + num2
-            elif operation == 'subtract':
-                result = num1 - num2
-            elif operation == 'multiply':
-                result = num1 * num2
-            elif operation == 'divide':
-                result = num1 / num2 if num2 != 0 else 'Erro: Divisão por zero'
-            else:
-                result = 'Operação inválida'
-        except ValueError:
-            result = 'Erro: Entrada inválida'
-    
-    return render_template('calculator.html', result=result)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+a = [x for x in input().lower()]
+b = []
+cont = 0
+for x in a:
+    if cont == 0 or (a[a.index(x)-1].isspace() and not a[a.index(x)+2].isspace()):
+        b.append(x.upper())
+    else:
+        b.append(x)
+    cont+=1
+b = ''.join(b)
+print(b)
